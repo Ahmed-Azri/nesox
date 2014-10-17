ff6b94e8a3b75193e6aeafd482a1299c56336fad
heuristack <heuristack@gmail.com>
2014-10-17 12:13:40 +0800

    nesox C part released\!

--- obsolete/logging_test.cc
+++
@@ -1,7 +0,0 @@
-#include "logging.h"
-
-int main() {
-  InitializeLogger("../logs/info.log", "../logs/warn.log", "../logs/erro.log");
-  LOG(INFO)    << "An info message going into info.log";
-  return 0;
-}
