99fca927aa4dcc598cee3f27bad7756204dc9434
heuristack <heuristack@gmail.com>
2014-10-20 21:59:06 +0800

    change test log level

--- bin/testnesox.sh
+++ bin/testnesox.sh
@@ -3,6 +3,6 @@ source nesox.sh
 for port in $ports
 do
 	echo -e "${RED}test nesox on port $port ... ${RESTORE}"
-	nesox -l trace -r reader -s 1024 $host $port
+	nesox -l stats -r reader -s 1024 $host $port
 	echo -e "${GREEN}OK!!!${RESTORE}"
 done
