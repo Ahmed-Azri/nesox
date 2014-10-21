3754d621fc2691feb2a16b848ce94baafc0ab3bc
heuristack <heuristack@gmail.com>
2014-10-21 22:46:36 +0800

    do it fast

--- bin/collectdata.sh
+++ bin/collectdata.sh
@@ -3,8 +3,8 @@ source nesox.sh
 for node in $nodes
 do
 	echo -e "${RED}collect nesox data from $node ... ${RESTORE}"
-	sshcommand1="scp -r $user@$node:$logs $home/stat/$node-logs"
-	sshcommand2="scp -r $user@$node:$logs/* $home/stat/"
+	sshcommand1="scp -r $user@$node:$logs $home/sta1t/$node-logs"
+	sshcommand2="scp -r $user@$node:$logs/* $home/stat2/"
 	coproc $sshcommand1
 	wait $!
 	coproc $sshcommand2
