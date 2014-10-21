597ad9cb16cae9275b4291b8601fb8e97e816836
heuristack <heuristack@gmail.com>
2014-10-21 20:03:37 +0800

    do it fast

--- bin/collectdata.sh
+++ bin/collectdata.sh
@@ -3,5 +3,5 @@ source nesox.sh
 for node in $nodes
 do
 	echo -e "${RED}collect nesox data from $node ... ${RESTORE}"
-	scp -r $user@$node:$logs $home/stat/"$node-logs"
+	scp -r $user@$node:$logs/* $home/stat/
 done
