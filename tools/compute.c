f88c40c56db6eed684364ae3a12719885dc86425
heuristack <heuristack@gmail.com>
2014-10-22 14:48:47 +0800

    do it fast

--- tools/compute.c
+++ tools/compute.c
@@ -1,13 +1,17 @@
 #include "nesox.h"
 
-static char *getstime(char *line);
-static char *getetime(char *line);
-static char *getint(char *line);
-static char *getsindex(char *line);
-static char *getdindex(char *line);
-static char *getdatasize(char *line);
-
 static char *advance(char *line, char *pattern);
+
+typedef struct flowstat {
+	int findex;
+	char sindex, dindex;
+	long datasize;
+	double stime, etime, interval;
+} flow;
+
+static int printflow(flow f);
+static flow parseflow(char *s, flow *f);
+static flow flowinit(int index);
 
 int main(int argc, char *argv[])
 {
@@ -19,10 +23,16 @@ int main(int argc, char *argv[])
 
 	for (int i = 0; (linelen = getline(&line, &capacity, stdin)) > 0; i++) {
 		fprintf(stderr, "%ld/%ld: %s", linelen, capacity, line);
-		char *p = strstr(line, "[STATS]");
-		if (p) fprintf(stderr, "%s", p);
-		char *q = advance(line, "[STATS]");
-		if (q) fprintf(stderr, "%s", q);
+
+		char *temp = advance(line, "[STATS]");
+		char *this = advance(temp, "):");
+
+		flow f = flowinit(i+1);
+		parseflow(this, &f);
+		fprintf(stderr, "%d: ", f.findex);
+		printflow(f);
+
+
 	}
 
 	return 0;
@@ -33,4 +43,39 @@ char *advance(char *line, char *pattern)
 	char *p = strstr(line, pattern);
 	if (p) p += strlen(pattern);
 	return p;
+}
+
+int printflow(flow f)
+{
+return fprintf(stderr, "%c:%c:%ld:%0.8f:%0.8f:%0.8f\n",
+	f.sindex, f.dindex, f.datasize, f.stime, f.etime, f.interval);
+}
+
+flow parseflow(char *s, flow *p)
+{
+	flow f;
+	char *string = strdup(s);
+	char *token = NULL;
+	for (int t = 0; (token = strsep(&string, ":\n")) != NULL && t < 6; t++)
+	{
+		if (token) fprintf(stderr, "%d: %s\n", t, token);
+		switch(t)
+		{
+			case 0: f.sindex = token[0]; break;
+			case 1: f.dindex = token[0]; break;
+			case 2: f.datasize = atol(token); break;
+			case 3: f.stime = atof(token); break;
+			case 4: f.etime = atof(token); break;
+			case 5: f.interval = atof(token); break;
+			default: break;
+		}
+	}
+	f.findex = p->findex;
+	*p = f;
+	return f;
+}
+
+flow flowinit(int index)
+{
+	flow f; f.findex = index; return f;
 }
