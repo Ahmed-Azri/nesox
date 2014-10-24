204e8b349de88c4ddd8a35956fcb8496d74360c6
heuristack <heuristack@gmail.com>
2014-10-24 15:50:09 +0800

    do it fast

--- bin/synctime.sh
+++ bin/synctime.sh
@@ -2,9 +2,9 @@ source nesox.sh
 
 for node in $nodes
 do
-	echo "${BLUE}synctime on $node${RESTORE}"
+	echo -e "${BLUE}synctime on $node${RESTORE}"
 	timestring=`date`
-	shellcommand="date --set="$timestring""
+	shellcommand='date --set="$timestring"'
 	sshcommand="ssh $user@$node $shellcommand"
 	coproc $sshcommand
 	wait $!
