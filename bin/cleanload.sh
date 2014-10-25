162626197b694d2d12994df0817b302c47ba6536
heuristack <heuristack@gmail.com>
2014-10-25 23:55:46 +0800

    do it fast

--- bin/cleanload.sh
+++ bin/cleanload.sh
@@ -1,6 +1,11 @@
 source nesox.sh
 
+rm -rf $home/stat/*
+
 command="rm -rf $home/loadlogs/*"
+$command
+
+if [ "$os" != Linux ]; then exit 0; fi;
 
 for node in $nodes
 do
@@ -8,5 +13,3 @@ do
 	echo $command
 	ssh $user@$node $command
 done
-
-rm -rf $home/stat/*
