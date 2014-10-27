6232b491723e6c56b5017629520a58d1ebaeff84
heuristack <heuristack@gmail.com>
2014-10-27 13:13:23 +0800

    do it fast

--- sched/README.md
+++ sched/README.md
@@ -3,4 +3,12 @@ Nesox Scheduling Module
 
 **Nesox scheduling module is actually a set of Ryu application implemented scheduling algorithms.**
 
+For each functions, develop an app, then in the paper, for each app, there can be a paragraph.
+But the precondition is that multiple apps can work cooperately in ryu-manager app-list.
+
+
+
+
+
+
 
---
+++ sched/maclearner.py
@@ -0,0 +1,24 @@
+from ryu import utils
+from ryu.base import app_manager
+from ryu.ofproto import ofproto_v1_3
+from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import HANDSHAKE_DISPATCHER, CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller import ofp_event
+
+
+class MACLEARNER(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(NesoxTest03, self).__init__(*args, **kwargs)
+
+    def send_echo_request(self, datapath, data):
+        ofp = datapath.ofproto
+        ofp_parser = datapath.ofproto_parser
+        req = ofp_parser.OFPEchoRequest(datapath, data)
+        datapath.send_msg(req)
+
+    @set_ev_cls(ofp_event.EventOFPEchoRequest, [HANDSHAKE_DISPATCHER, CONFIG_DISPATCHER, MAIN_DISPATCHER])
+    def echo_request_handler(self, ev):
+        self.logger.debug('OFPEchoRequest received: data=%s', utils.hex_array(ev.msg.data))
+
