b29d0ca7ef4da3f703edca998e30cde60148f938
heuristack <heuristack@gmail.com>
2014-10-31 01:12:21 +0800

    do it fast

--- bin/loadall.sh
+++ bin/loadall.sh
@@ -4,12 +4,23 @@ cd $load
 
 loadfiles=*.ld
 if [ "$1" != "" ]; then towait="$1"; else towait=30; fi
+if [ "$2" != "" ]; then interactive="$2"; else interactive=""; fi
 
-echo -e "${YELLOW}please wait for $towait second(s)${RESTORE}"
-sleep $towait
 
 for loadfile in $loadfiles
 do
+
+if [ "$interactive" != "" ];
+then
+echo -e "${YELLOW}Before press any key to contunue: disable Openflow${RESTORE}"
+read waitforinput
+fi
+
 echo -e "${YELLOW}process load: $loadfile${RESTORE}"
 loadbench.sh -f $loadfile
+
+echo -e "${YELLOW}Quickly enable Openflow Instance within 5 seconds!!!${RESTORE}"
+echo -e "${YELLOW}please wait for $towait second(s)${RESTORE}"
+sleep $towait
+
 done
