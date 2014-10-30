0031b8be7ec09ba2a686c1c3df44f2988407c585
heuristack <heuristack@gmail.com>
2014-10-31 00:46:16 +0800

    do it fast

--- bin/loadall.sh
+++ bin/loadall.sh
@@ -5,10 +5,11 @@ cd $load
 loadfiles=*.ld
 if [ "$1" != "" ]; then towait="$1"; else towait=30; fi
 
+echo -e "${YELLOW}please wait for $towait second(s)${RESTORE}"
+sleep $towait
+
 for loadfile in $loadfiles
 do
 echo -e "${YELLOW}process load: $loadfile${RESTORE}"
 loadbench.sh -f $loadfile
-echo -e "${YELLOW}please wait for $towait second(s)${RESTORE}"
-sleep $towait
 done
