c1cb71d5b74e6e7d7c06e7fe443ba06a9e84f20d
heuristack <heuristack@gmail.com>
2014-10-31 00:18:35 +0800

    do it fast

--- bin/generatestat.sh
+++ bin/generatestat.sh
@@ -10,13 +10,13 @@ do
 	nameposfix="com$comparg"
 	outputname="$stat/$statname-$nameposfix.stat"
 
+	echo "$statname-$nameposfix" > $outputname
 	for mean in $(seq -w 100 100 1600)
 	do
 		if [ "$statname" = "all2all" ]; then variance="0000"; fi
 		if [ "$statname" = "shuffle" ]; then variance="$mean"; fi
 		nameprefix="norm-$variance-$mean"
 		inputnames="$stat/$nameprefix*$nameposfix"
-		echo "$statname-$nameposfix" > $outputname
 
 		x=$mean
 		for inputname in $inputnames
