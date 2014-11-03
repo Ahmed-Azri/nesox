07c0ca4e345647e586c6fc3ce4b93626666735ba
heuristack <heuristack@gmail.com>
2014-11-03 22:52:32 +0800

    finish it

--- sched/maclearner.py
+++ sched/maclearner.py
@@ -120,7 +120,8 @@ class MACLEARNER(app_manager.RyuApp):
         todo: if ipv4.dst == 202.45.128.181: output port
         """
         controlleraddress = ipv4.ipv4(dst="202.45.128.181")
-        if ip4.dst == controlleraddress.dst:
+        # if ip4.dst == controlleraddress.dst:
+        if ip4.dst == "202.45.128.181":
             out_port = 9
 
         actions = [parser.OFPActionOutput(out_port)]
