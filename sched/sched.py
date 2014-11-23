1baa617463a6fca595834c02422e60b2ccc75fae
heuristack <heuristack@gmail.com>
2014-11-23 14:33:40 +0800

    finish it

---
+++ sched/sched.py
@@ -0,0 +1,33 @@
+from ryu import utils
+from ryu.base import app_manager
+from ryu.ofproto import ofproto_v1_3
+from ryu.controller import ofp_event
+from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import HANDSHAKE_DISPATCHER, CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.lib.packet import packet, ethernet, ipv4
+
+from os import listdir
+from nesox import flow
+from nesox import transdir
+
+class SCHED(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SCHED, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, event):
+        self.logger.info("SCHED: Handler = Switch Features: enter!")
+
+        flows = []
+        transfermap = {}
+        transfers = listdir(transdir)
+
+        for tran in transfers: transfile = open(transdir + tran)
+        for line in transfile: flows.append(flow(int(line[0]), int(line[2]), int(line[6:].rstrip()), int(line[4])))
+        for flow in flows: transfermap[(flow.source, flow.destination)] = flow
+
+        transfermap[(1,2)].show(1)
+
+        self.logger.info("SCHED: Handler = Switch Features: leave!")
