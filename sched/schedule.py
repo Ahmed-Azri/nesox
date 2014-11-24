6f7f69feecd0b9658b5774957e1303b3c39832d0
heuristack <heuristack@gmail.com>
2014-11-24 15:37:30 +0800

    finish it

--- sched/schedule.py
+++ sched/schedule.py
@@ -19,12 +19,30 @@ class SCHEDULE(app_manager.RyuApp):
         super(SCHEDULE, self).__init__(*args, **kwargs)
         self.debug = 1
         self.hard_table_id = 100
+        self.hard_table_no = 1
+        self.hard_tables = [100]
         self.soft_table_id = 200
         self.soft_table_no = 4
+        self.soft_tables = [200, 201, 202, 203]
         self.flows = []
         self.transfermap = {}
         self.transfers = listdir(transdir)
+        self.datapath = None
 
+    def insert_output(self, datapath=self.datapath, tid, match, pri, port):
+        protocol = datapath.ofproto
+        parser = datapath.ofproto_parser
+        actions = [parser.OFPActionOutput(port)]
+        instructions = [parser.OFPInstructionActions(protocol.OFPIT_APPLY_ACTIONS, action)]
+        modification = parser.OFPFlowMod(datapath=datapath, table_id=tid, match=match, priority=pri, instructions=instructions)
+        datapath.send_msg(modification)
+
+    def insert_goto(self, datapath=self.datapath, tid, match, pri, gototid):
+        protocol = datapath.ofproto
+        parser = datapath.ofproto_parser
+        instructions = [parser.OFPInstructionGotoTable(gotoid)]
+        modification = parser.OFPFlowMod(datapath=datapath, table_id=tid, match=match, priority=pri, instructions=instructions)
+        datapath.send_msg(modification)
 
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -32,7 +50,13 @@ class SCHEDULE(app_manager.RyuApp):
         self.logger.info("SCHEDULE: Handler = Switch Features: enter!")
 
         """
-        read the transfer requirement into a `list` and a `dictionary`
+        initialize datapath
+        """
+        datapath = event.msg.datapath
+        self.datapath = datapath
+
+        """
+        read `transfer` requirement into a `list` and a `dictionary`
         """
         for tran in self.transfers: transfile = open(transdir + tran)
         for line in transfile: self.flows.append(nesox.flow(int(line[0]), int(line[2]), int(line[6:].rstrip()), int(line[4])))
@@ -40,12 +64,16 @@ class SCHEDULE(app_manager.RyuApp):
         if (self.debug): self.logger.info("tramsfermap: %s", self.transfermap)
 
         """
-        initialize the pipeline
+        initialize pipeline
         """
-        datapath = event.msg.datapath
-        protocol = datapath.ofproto
-        prparser = datapath.ofproto_parser
 
+        tid = 100
+        for gototid in self.soft_tables:
+            m = parser.OFPMatch()
+            p = 4
+            self.insert_goto(datapath, tid, m, p, gototid)
+            tid = gototid
 
         self.logger.info("SCHEDULE: Handler = Switch Features: leave!")
+
 
