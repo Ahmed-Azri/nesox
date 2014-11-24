8347b67e51e59cc12e74a1f5272ed5b8d19fbe09
heuristack <heuristack@gmail.com>
2014-11-24 23:40:12 +0800

    finish it

--- sched/schedule.py
+++ sched/schedule.py
@@ -231,7 +231,7 @@ class SCHEDULE(app_manager.RyuApp):
             if stat.priority == 1:
                 counters.append((stat.table_id, stat.match, stat.priority, stat.packet_count, stat.byte_count))
         if self.debug: self.logger.info("counters: %s", counters)
-        self.packet_size = (counters[0][4] - self.byte_count) / (counters[0][3] - self.packet_count)
+        if counters[0][3] != 0: self.packet_size = (counters[0][4] - self.byte_count) / (counters[0][3] - self.packet_count)
         if self.debug: self.logger.info("counters: %s", counters)
         if self.debug: self.logger.info("packet size: %s", self.packet_size)
 
