4cef81665c052aef409cbd40f9f076ea46fa41e0
heuristack <heuristack@gmail.com>
2014-11-25 11:59:41 +0800

    finish it

--- sched/schedule.py
+++ sched/schedule.py
@@ -312,10 +312,11 @@ class SCHEDULE(app_manager.RyuApp):
         caculate packet size
         """
         if (counters) and (counters[-1][4] != self.packet_count):
-            self.packet_size = (counters[0][5] - self.byte_count) / (counters[0][4] - self.packet_count)
+            self.packet_size = (counters[-1][5] - self.byte_count) / (counters[-1][4] - self.packet_count)
         if self.debug: self.logger.info("packet size: %s", self.packet_size)
-        self.byte_count = counters[0][5]
-        self.packet_count = counters[0][4]
+        if counters:
+            self.byte_count = counters[-1][5]
+            self.packet_count = counters[-1][4]
 
         """
         request `reading counter`
