5dc56b723134259a14680382519483a5f7f7da35
heuristack <heuristack@gmail.com>
2014-11-26 12:03:58 +0800

    finish it

--- sched/schedule.py
+++ sched/schedule.py
@@ -361,11 +361,11 @@ class SCHEDULE(app_manager.RyuApp):
             if self.debug: self.logger.info("counter: %s", counter)
             match = counter[1]
             if self.debug: self.logger.info("match: %s", match)
-            if (hasattr(match, 'eth_type')) and (match.eth_type == 0x0800):
+            if (hasattr(match.oxm_fields, 'eth_type')) and (match.eth_type == 0x0800):
                 if self.debug: self.logger.info("Yes ......!!!")
                 if self.debug: self.logger.info("counter: %s", counter)
-                if (hasattr(match, 'ipv4_src')): s = match.ipv4_src[-1]
-                if (hasattr(match, 'ipv4_dst')): d = match.ipv4_dst[-1]
+                if (hasattr(match.oxm_fields, 'ipv4_src')): s = match.ipv4_src[-1]
+                if (hasattr(match.oxm_fields, 'ipv4_dst')): d = match.ipv4_dst[-1]
         if self.debug: self.logger.info("flow: (%s)>>(%s)", s, d)
 
 
